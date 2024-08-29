05804342628590ef6891293ed9ade685f99a4287
Michael Rodrigues <34792032+Michaelbrodrigues@users.noreply.github.com>
2024-08-29 14:05:02 +1200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'https://bico-client.vercel.app',
+    origin: ['https://bico-client.vercel.app'],
     methods: ["GET"],
     allowedHeaders: ['Content-Type', 'Authorization'],
     maxAge: 600,
