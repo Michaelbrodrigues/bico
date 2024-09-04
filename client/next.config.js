850f38467507bc6ae7a02fbe56ead6277f8216b4
Michaelbrodrigues <michaelbrunorodrigues@gmail.com>
2024-09-05 07:27:56 +1200

    ud1

--- client/next.config.js
+++ client/next.config.js
@@ -1,16 +1,31 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-        port: "",
-        pathname: "**",
-      },
-    ],
+export default {
+  async headers() {
+      return [
+          {
+              // Apply headers to all routes
+              source: '/(.*)',
+              headers: [
+                  {
+                      key: 'Access-Control-Allow-Credentials',
+                      value: 'true',
+                  },
+                  {
+                      key: 'Access-Control-Allow-Origin',
+                      // Replace with your domain
+                      value: 'https://bico-client.vercel.app',
+                  },
+                  {
+                      key: 'Access-Control-Allow-Methods',
+                      value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+                  },
+                  {
+                      key: 'Access-Control-Allow-Headers',
+                      value:
+                          
+'X-CSRF-Token, X-Requested-With, Accept, Accept- Version, Content - Length, Content - MD5, Content - Type, Date, X - Api - Version',
+                  },
+              ],
+          },
+      ];
   },
 };
-
-module.exports = nextConfig;
