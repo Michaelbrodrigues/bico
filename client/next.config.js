8eb735475a0960be4d5918721d2667a783496971
Michaelbrodrigues <michaelbrunorodrigues@gmail.com>
2024-09-05 07:42:21 +1200

    ud2

--- client/next.config.js
+++ client/next.config.js
@@ -1,31 +1,46 @@
-export default {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
+
+  // Adding the headers method to handle CORS
   async headers() {
-      return [
+    return [
+      {
+        // Apply headers to all routes
+        source: '/(.*)',
+        headers: [
           {
-              // Apply headers to all routes
-              source: '/(.*)',
-              headers: [
-                  {
-                      key: 'Access-Control-Allow-Credentials',
-                      value: 'true',
-                  },
-                  {
-                      key: 'Access-Control-Allow-Origin',
-                      // Replace with your domain
-                      value: 'https://bico-client.vercel.app',
-                  },
-                  {
-                      key: 'Access-Control-Allow-Methods',
-                      value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-                  },
-                  {
-                      key: 'Access-Control-Allow-Headers',
-                      value:
-                          
-'X-CSRF-Token, X-Requested-With, Accept, Accept- Version, Content - Length, Content - MD5, Content - Type, Date, X - Api - Version',
-                  },
-              ],
+            key: 'Access-Control-Allow-Credentials',
+            value: 'true',
           },
-      ];
+          {
+            key: 'Access-Control-Allow-Origin',
+            // Replace with your domain
+            value: 'https://bico-client.vercel.app',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
   },
 };
+
+module.exports = nextConfig;
