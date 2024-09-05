5ec5fa944b82568932f5abd488f32019bf85da56
Michaelbrodrigues <michaelbrunorodrigues@gmail.com>
2024-09-05 15:58:37 +1200

    hsdfh

--- client/next.config.js
+++ client/next.config.js
@@ -12,7 +12,33 @@ const nextConfig = {
     ],
   },
 
-  
+  // Adding the headers method to handle CORS only for API routes
+  async headers() {
+    return [
+      {
+        source: '/api/(.*)',  // Apply headers only to API routes
+        headers: [
+          {
+            key: 'Access-Control-Allow-Credentials',
+            value: 'true',
+          },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://bico-client.vercel.app', // Update with your frontend domain
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
