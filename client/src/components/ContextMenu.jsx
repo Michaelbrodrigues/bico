5bcf3ca79cf71ec88bd311b2a6d585c1724ca849
Michaelbrodrigues <michaelbrunorodrigues@gmail.com>
2024-09-14 13:26:21 +1200

    contextmenu ud

--- client/src/components/ContextMenu.jsx
+++ client/src/components/ContextMenu.jsx
@@ -5,9 +5,10 @@ function ContextMenu({ data }) {
   const router = useRouter();
   return (
     <div
-      className={`z-10  bg-white divide-y divide-gray-100  shadow-2xl border w-44 dark:bg-gray-700
-      fixed right-5 top-20 
-      `}
+      className="z-10 bg-white divide-y divide-gray-100 shadow-2xl border dark:bg-gray-700
+        fixed right-4 sm:right-5 top-16 sm:top-20
+        w-36 sm:w-44
+      "
     >
       <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
         {data.map(({ name, callback }, index) => {
@@ -15,13 +16,13 @@ function ContextMenu({ data }) {
             <li
               key={index}
               onClick={callback}
-              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer"
+              className="block px-3 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer"
             >
               {name}
             </li>
           );
         })}
-      </ul> 
+      </ul>
     </div>
   );
 }
