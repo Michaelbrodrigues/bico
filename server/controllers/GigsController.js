55bc263fd79b68d394154cf6c7477eaf655a16c5
Michael Rodrigues <34792032+Michaelbrodrigues@users.noreply.github.com>
2024-11-09 14:33:36 +1300

    uncomment gigscontroller

--- server/controllers/GigsController.js
+++ server/controllers/GigsController.js
@@ -3,52 +3,52 @@ import { PrismaClient } from "@prisma/client";
 import { existsSync, renameSync, unlinkSync } from "fs";
 
 export const addGig = async (req, res, next) => {
-  // try {
-  //   if (req.files) {
-  //     const fileKeys = Object.keys(req.files);
-  //     const fileNames = [];
-  //     fileKeys.forEach((file) => {
-  //       const date = Date.now();
-  //       renameSync(
-  //         req.files[file].path,
-  //         "uploads/" + date + req.files[file].originalname
-  //       );
-  //       fileNames.push(date + req.files[file].originalname);
-  //     });
-  //     if (req.query) {
-  //       const {
-  //         title,
-  //         description,
-  //         category,
-  //         features,
-  //         price,
-  //         revisions,
-  //         time,
-  //         shortDesc,
-  //       } = req.query;
-  //       const prisma = new PrismaClient();
-  //       await prisma.gigs.create({
-  //         data: {
-  //           title,
-  //           description,
-  //           deliveryTime: parseInt(time),
-  //           category,
-  //           features,
-  //           price: parseInt(price),
-  //           shortDesc,
-  //           revisions: parseInt(revisions),
-  //           createdBy: { connect: { id: req.userId } },
-  //           images: fileNames,
-  //         },
-  //       });
-  //       return res.status(201).send("Successfully created the gig.");
-  //     }
-  //   }
-  //   return res.status(400).send("All properties should be required.");
-  // } catch (err) {
-  //   console.log(err);
-  //   return res.status(500).send("Internal Server Error");
-  // }
+  try {
+    if (req.files) {
+      const fileKeys = Object.keys(req.files);
+      const fileNames = [];
+      fileKeys.forEach((file) => {
+        const date = Date.now();
+        renameSync(
+          req.files[file].path,
+          "uploads/" + date + req.files[file].originalname
+        );
+        fileNames.push(date + req.files[file].originalname);
+      });
+      if (req.query) {
+        const {
+          title,
+          description,
+          category,
+          features,
+          price,
+          revisions,
+          time,
+          shortDesc,
+        } = req.query;
+        const prisma = new PrismaClient();
+        await prisma.gigs.create({
+          data: {
+            title,
+            description,
+            deliveryTime: parseInt(time),
+            category,
+            features,
+            price: parseInt(price),
+            shortDesc,
+            revisions: parseInt(revisions),
+            createdBy: { connect: { id: req.userId } },
+            images: fileNames,
+          },
+        });
+        return res.status(201).send("Successfully created the gig.");
+      }
+    }
+    return res.status(400).send("All properties should be required.");
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send("Internal Server Error");
+  }
 };
 
 // Function to delete a gig
@@ -135,59 +135,59 @@ export const getGigData = async (req, res, next) => {
 };
 
 export const editGig = async (req, res, next) => {
-  // try {
-  //   if (req.files) {
-  //     const fileKeys = Object.keys(req.files);
-  //     const fileNames = [];
-  //     fileKeys.forEach((file) => {
-  //       const date = Date.now();
-  //       renameSync(
-  //         req.files[file].path,
-  //         "uploads/" + date + req.files[file].originalname
-  //       );
-  //       fileNames.push(date + req.files[file].originalname);
-  //     });
-  //     if (req.query) {
-  //       const {
-  //         title,
-  //         description,
-  //         category,
-  //         features,
-  //         price,
-  //         revisions,
-  //         time,
-  //         shortDesc,
-  //       } = req.query;
-  //       const prisma = new PrismaClient();
-  //       const oldData = await prisma.gigs.findUnique({
-  //         where: { id: req.params.gigId },
-  //       });
-  //       await prisma.gigs.update({
-  //         where: { id: req.params.gigId },
-  //         data: {
-  //           title,
-  //           description,
-  //           deliveryTime: parseInt(time),
-  //           category,
-  //           features,
-  //           price: parseInt(price),
-  //           shortDesc,
-  //           revisions: parseInt(revisions),
-  //           createdBy: { connect: { id: req.userId } },
-  //           images: fileNames,
-  //         },
-  //       });
-  //       oldData?.images.forEach((image) => {
-  //         if (existsSync(`uploads/${image}`)) unlinkSync(`uploads/${image}`);
-  //       });
-  //       return res.status(201).send("Successfully Eited the gig.");
-  //     }
-  //   }
-  //   return res.status(400).send("All properties should be required.");
-  // } catch (err) {
-  //   console.log(err);
-  //   return res.status(500).send("Internal Server Error");
-  // }
+  try {
+    if (req.files) {
+      const fileKeys = Object.keys(req.files);
+      const fileNames = [];
+      fileKeys.forEach((file) => {
+        const date = Date.now();
+        renameSync(
+          req.files[file].path,
+          "uploads/" + date + req.files[file].originalname
+        );
+        fileNames.push(date + req.files[file].originalname);
+      });
+      if (req.query) {
+        const {
+          title,
+          description,
+          category,
+          features,
+          price,
+          revisions,
+          time,
+          shortDesc,
+        } = req.query;
+        const prisma = new PrismaClient();
+        const oldData = await prisma.gigs.findUnique({
+          where: { id: req.params.gigId },
+        });
+        await prisma.gigs.update({
+          where: { id: req.params.gigId },
+          data: {
+            title,
+            description,
+            deliveryTime: parseInt(time),
+            category,
+            features,
+            price: parseInt(price),
+            shortDesc,
+            revisions: parseInt(revisions),
+            createdBy: { connect: { id: req.userId } },
+            images: fileNames,
+          },
+        });
+        oldData?.images.forEach((image) => {
+          if (existsSync(`uploads/${image}`)) unlinkSync(`uploads/${image}`);
+        });
+        return res.status(201).send("Successfully Eited the gig.");
+      }
+    }
+    return res.status(400).send("All properties should be required.");
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send("Internal Server Error");
+  }
 };
 
 export const searchGigs = async (req, res, next) => {
